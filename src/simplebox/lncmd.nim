4d1b31a858002e203d2609aa031d9d348e32c919
Kreato <kreato@kreato.dev>
2023-10-01 14:31:43 +0300

    implement ln command

--- src/simplebox/lncmd.nim
+++ src/simplebox/lncmd.nim
@@ -1,2 +1,25 @@
-proc ln*(source: string, destination: string) =
+import os
+
+proc ln*(f = false, s = false, sourceAndDestination: seq[string]) =
   ## Create a link.
+  
+  if sourceAndDestination.len == 0:
+    echo "ln: missing source and destination"
+    quit(1)
+
+  let destination = sourceAndDestination[1]
+  let source = sourceAndDestination[0]
+
+  if (fileExists(destination) or dirExists(destination)) and not f:
+    echo "ln: failed to create symbolic link '"&destination&"': File exists"
+    quit(1)
+  elif f:
+    if fileExists(destination):
+      removeFile(destination)
+    elif dirExists(destination):
+      removeDir(destination)
+
+  if s:
+    createSymlink(source, destination)
+  else:
+    createHardlink(source, destination)
